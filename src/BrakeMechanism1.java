d114ef2dd07b27e8b5f1a394a8b2b3ad3edff4d6
OlegGeivandov <140657755+OlegGeivandov@users.noreply.github.com>
2023-10-03 21:08:16 +0300

    1

--- src/BrakeMechanism1.java
+++ src/BrakeMechanism1.java
@@ -1,2 +1,71 @@
-package PACKAGE_NAME;public class BrakeMechanism1 {
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+public class BrakeMechanism1 {
+    ArrayList<Integer> firstBrakePad;
+    ArrayList<Integer> secondBrakePad;
+    int minimumLegalBrakeLength;
+
+    public BrakeMechanism1(ArrayList<Integer> firstBrakePad, ArrayList<Integer> secondBrakePad) {
+        if (firstBrakePad.size() >= secondBrakePad.size()) {
+            this.firstBrakePad = firstBrakePad;
+            this.secondBrakePad = secondBrakePad;
+        }
+        else {
+            this.firstBrakePad = secondBrakePad;
+            this.secondBrakePad = firstBrakePad;
+        }
+    }
+
+    int calculateMinimumBrakeLength(){
+        int firstPadSize = firstBrakePad.size();
+        int secondPadSize = secondBrakePad.size();
+        int firstAndSecondSize = firstPadSize + secondPadSize; // размерность = 1-я колодка + вторая
+        int firstAndTwoSecondSize = firstPadSize + 2*secondPadSize; // размерность = 1-я колодка + 2 вторых
+
+        // увеличенная первая колодка, заполненная 0
+        ArrayList<Integer> extendedFirstPad = new ArrayList<>(Collections.nCopies(firstAndTwoSecondSize-1, 0));
+
+        // заполняем увеличенную первую колодку
+        for (int i = secondPadSize; i < firstAndSecondSize; i++) {
+            extendedFirstPad.set(i, firstBrakePad.get(i-secondPadSize));
+        }
+        System.out.println(extendedFirstPad + " extendedFirstPad");
+
+        int minSize = firstAndSecondSize;
+
+        // сдвигаем вторую колодку относительно первой
+        for (int i = 0; i < firstAndSecondSize; i++) {
+            // увеличенная вторая колодка, заполненная 0
+            ArrayList<Integer> extendedSecondPad = new ArrayList<>(Collections.nCopies(firstAndTwoSecondSize-1, 0));
+
+            // заполняем увеличенную вторую колодку
+            for (int n = i; n < secondPadSize+i; n++) {
+                extendedSecondPad.set(n, secondBrakePad.get(n-i));
+            }
+
+            // результат наложения двух расширенных колодок
+            ArrayList<Integer> bothPad = new ArrayList<>(Collections.nCopies(firstAndTwoSecondSize-1, 0));
+            for (int j = 0; j < firstAndTwoSecondSize-1; j++) {
+                bothPad.set((j), (extendedFirstPad.get(j)+extendedSecondPad.get(j)));
+            }
+            System.out.println(bothPad + " bothPad   i=" + i);
+
+            // расчет размера данной конфигурации наложения
+            if (bothPad.stream().allMatch(x->x<=3)){ // если все элементы наложения не больше 3
+                int minSizeTemp = 0;
+
+                for (int j = 0; j < firstAndTwoSecondSize-1; j++){ // проходим по всем элементам
+                    if (bothPad.get(j)!=0) minSizeTemp++; // если элемент отличается от 0
+                }
+                if (minSizeTemp < minSize) minSize = minSizeTemp;
+                System.out.println(minSizeTemp + " minSizeTemp      " + minSize + " minSize    " + bothPad.stream().allMatch(x->x<=3));
+            }
+        }
+        minimumLegalBrakeLength = minSize;
+        return minSize;
+    }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,41 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
 public class Main {
+    public static BrakeMechanism1 bm;
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        String fileName = "1.txt";
+        readFromFile(fileName);
+        bm.calculateMinimumBrakeLength();
+        System.out.println(bm.minimumLegalBrakeLength + " bm.minimumLegalBrakeLength");
+    }
+
+    static void readFromFile (String fileName) {
+        try {
+            Scanner scanner = new Scanner(new File(fileName));
+            bm = new BrakeMechanism1(getArrayListFromString(scanner.nextLine()), getArrayListFromString(scanner.nextLine()));
+            scanner.close();
+            System.out.println(bm.firstBrakePad + " bm.firstBrakePad");
+            System.out.println(bm.secondBrakePad + " bm.secondBrakePad");
+        }
+        catch (FileNotFoundException e) {
+            System.out.println("упс, файл "+fileName+" не найден");
+        }
+        catch (InputMismatchException e){
+            System.out.println("в файле не совсем число записано");
+        }
+    }
+
+    public static ArrayList<Integer> getArrayListFromString(String str) {
+        ArrayList<Integer> list = new ArrayList<>();
+        char[] charArray = str.toCharArray();
+        for (Character ch : charArray) {
+            list.add(Integer.parseInt(String.valueOf(ch)));
+        }
+        return list;
     }
 }
